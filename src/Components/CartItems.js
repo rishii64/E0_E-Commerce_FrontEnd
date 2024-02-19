eaf6ce00b0f6481c690a2213da300d41ef672cb9
rishii64 <saptarsismmitra@gmail.com>
2024-02-19 16:48:28 +0530

    updated

--- src/Components/CartItems.js
+++ src/Components/CartItems.js
@@ -59,9 +59,9 @@ export default function CartItems() {
                     <img className='cartImg' src={item.images.LinkOne} alt="" />
 
                     <div className="prodDetails">
-                      <h2 className='cartTitle'>{item.title.slice(0, 22)}</h2>
+                      <h3 className='cartTitle'>{item.title.slice(0, 22)}</h3>
                       <p> ₹{item.Dprice}</p>
-                      <div>
+                      <div className='modification'>
                         <button className='btnDecrement' onClick={() => handleDecrement(item.id)}>-</button>
                         <span> {item.cartQuantity} </span>
                         <button className='btnIncrement' onClick={() => handleIncrement(item.id)}>+</button>
